2f6172d1528a905a6350c0c5934d083395dde21d
Sebastien M. Popoff <sebastien.popoff@espci.fr>
2022-04-28 17:15:20 +0200

    layout changes

--- app/messages.py
+++ app/messages.py
@@ -15,44 +15,45 @@ def get_message_from_score(score):
         return choice(message_not_word)
 
 message_not_word = [
-    "C'est du du Polonais ?",
-    "Non mais sinon, tu veux pas essayer des mots français ?",
-    "C'est du français ?",
+    "C'est du du Polonais&nbsp;?",
+    "Non mais sinon, tu veux pas essayer des mots français&nbsp;?",
+    "C'est du français&nbsp;?",
     "Aussi vrai que <i>ptdr</i> c'est du français",
     "N'oublie pas que ça doit être au singulier masculin...",
-    "T'as pas un petit cousin de 6 ans qui peut te proposer des vrais mots ?",
-    "¿ Hablas francés ?",
-    "ты говоришь по французски ?",
+    "T'as pas un petit cousin de 6 ans qui peut te proposer des vrais mots&nbsp;?",
+    "¿&nbsp;Hablas francés&nbsp;?",
+    "ты говоришь по французски&nbsp;?",
     "Demande des conseils d'orthographe à Trump."
 ]
 
 messages_not_top1000 = [
     "Pas dans le top 1000, et pas top du tout",
-    "Ce n'est pas dans le top 1000 (ni dans le top 1 000 000 !)",
+    "Ce n'est pas dans le top 1000 (ni dans le top 1000000&nbsp;!)",
     "Ouais mais en fait tu n'y est pas du tout...",
-    "Sinon, t'as pas envie de vraiment essayer ?",
-    "Oulalalala, tu y étais presque ! À quelque milliers de place près c'était bon !",
-    "Genre, ça n'a rien à voir !",
-    "Alors là, je dis non !",
+    "Sinon, t'as pas envie de vraiment essayer&nbsp;?",
+    "Oulalalala, tu y étais presque&nbsp;! À quelque milliers de place près c'était bon&nbsp;!",
+    "Genre, ça n'a rien à voir&nbsp;!",
+    "Alors là, je dis non&nbsp;!",
     "Mais clairement pas...",
     "Trop à l'ouest.",
     "Touche le fond mais creuse encore.",
     "Aussi logique que Macron à la fête de l'huma.",
-    "Tu est probablement doué·e à plein d'autres activités !",
+    "Tu est probablement doué·e à plein d'autres activités&nbsp;!",
     "Rien. n'a. voir.",
+    "Essaie encore&nbsp;!"
 ]
 
 message_success = [
-    "Trop de la balle !",
-    "Avoue ! Tu as triché ?",
+    "Trop de la balle&nbsp;!",
+    "Avoue ! Tu as triché&nbsp;?",
 ]
 
 messages_top10 = [
     "Pas mal du tout !",
-    "Encore un petit effort !",
+    "Encore un petit effort&nbsp;!",
     "Pas mal ! Et dire qu'il va te falloir encore 97 essais...",
-    "Chaud chaud chaud !!! (Cacao)",
-    "Tu es dans le top 10 !",
+    "Chaud chaud chaud&nbsp;!!! (Cacao)",
+    "Tu es dans le top 10&nbsp;!",
 ]
 
 messages_top100 = [

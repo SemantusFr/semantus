eff8514feec6c290c345fb79a4eb9225630977c9
Sebastien M. Popoff <sebastien.popoff@espci.fr>
2022-04-29 23:24:04 +0200

    improve hist

--- app/figures.py
+++ app/figures.py
@@ -11,13 +11,47 @@ params = {"ytick.color" : "w",
           "axes.edgecolor" : "w"}
 plt.rcParams.update(params)
 
-def get_hist_image(data_points):
+def get_hist_image(data_points, user_points = 400):
 
+    # plt.figure(figsize = (6,5))
+    # sns.histplot(data=data_points, element="step")
+    # plt.tick_params(axis='both', which='major', labelsize=14)
+    # plt.xlabel(r'Score', fontsize = 18)
+    # plt.ylabel(r'Joueurs', fontsize = 18)
+
+    binwidth = 100
     plt.figure(figsize = (6,5))
-    sns.histplot(data=data_points, element="step")
+    ax = sns.histplot(
+        # binwidth = binwidth,
+        data=data_points, 
+        element="step"
+    )
+
+    
     plt.tick_params(axis='both', which='major', labelsize=14)
     plt.xlabel(r'Score', fontsize = 18)
     plt.ylabel(r'Joueurs', fontsize = 18)
+
+    if user_points:
+        dx = ax.get_xlim()[1]-ax.get_xlim()[0]
+        dy = ax.get_ylim()[1]-ax.get_ylim()[0]
+
+        ind = 400
+        lenArrow = dy/2
+        lenHead = lenArrow/4
+        wiArrow = dx/20
+        n = lenArrow/2
+        plt.arrow(
+            ind, 
+        #     n+lenArrow+lenHead, 
+            lenArrow/20+lenArrow+lenHead,
+            0, 
+            -lenArrow, 
+            head_width=wiArrow*2.5, 
+            head_length=lenHead, 
+            width=wiArrow, 
+            fc='g', ec='k')
+        
     # plt.xlim([0, 1000])
     
     my_stringIObytes = io.BytesIO()

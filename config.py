4c9f0c05292ce732d426dae2a9e166ba55e1d1b3
Sebastien M. Popoff <sebastien.popoff@espci.fr>
2022-04-30 11:30:49 +0200

    date

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 't@mereen$tring48201dfa4dfgsdasaf4724]['
-    DAY0 = (2022, 4, 24)
+    DAY0 = (2022, 4, 28)

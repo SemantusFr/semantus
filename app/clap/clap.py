a8904cbf189b107fcb10c8b206f2143c4a806a91
Sebastien M. Popoff <sebastien.popoff@espci.fr>
2022-05-24 20:55:00 +0200

    save stats to server db

--- app/clap/clap.py
+++ app/clap/clap.py
@@ -20,7 +20,9 @@ from app.common import (
     connect_to_db,
     clean_word,
     hash,
-    COLORS
+    get_hash_client_ip,
+    COLORS,
+    STAT_DB_PATH
 )
 
 CLAP_DB_PATH = f"{Path(__file__).parent.parent.parent}/clap.db"
@@ -56,6 +58,77 @@ def clap():
         # game_sub_title = game_catch_phrase,
         colors = COLORS,
     )
+
+def get_score(nb_guesses, nb_title_guesses):
+    score = 1000-5*nb_guesses-15*nb_title_guesses
+    return score if score > 0 else 0
+
+@clap_bp.route('/win')
+def link_win():
+    '''
+    Update the database to add the win.
+    '''
+    title = request.args.get('title')
+    nb_guesses = int(request.args.get('nb_guesses'))
+    nb_title_guesses = int(request.args.get('nb_title_guesses'))
+    user_id = request.args.get('user_id')
+    score = get_score(nb_guesses, nb_title_guesses)
+    puzzleNumber = get_puzzle_number()
+    real_title = get_movie_info(puzzleNumber)[0]
+    already_won = False
+    if not (real_title == title):
+        return jsonify({})
+
+    ip_hash = get_hash_client_ip()
+    user_hash = hash(user_id)
+    unique_hash = ip_hash+user_hash
+    table_name = f"clap_day{puzzleNumber}"
+    con, cur = connect_to_db(STAT_DB_PATH)
+    con.commit()
+    query = f"create table if not exists {table_name}"
+    query += "(unique_hash TEXT PRIMARY KEY, ip_hash TEXT, user_hash TEXT, nb_guesses INT, nb_title_guesses INT, points INT)"
+    cur.execute(query)
+    con.commit()
+    with con:
+        query = f'SELECT * FROM clap_day{puzzleNumber} WHERE unique_hash = "{unique_hash}"'
+        cur.execute(query)
+        res = cur.fetchall()
+        if len(res) > 0:
+            already_won = True
+        else:
+            with con:
+                query = f"insert into clap_day{puzzleNumber} (unique_hash, ip_hash, user_hash, nb_guesses, nb_title_guesses, points)"
+                query += f"values (\"{unique_hash}\", \"{ip_hash}\", \"{user_hash}\", {nb_guesses}, {nb_title_guesses}, {score})"""
+                cur.execute(query)
+                con.commit()
+        query = f"SELECT * FROM clap_day{puzzleNumber}"
+        cur.execute(query)
+        res = cur.fetchall()
+        data = {
+            'already_won': already_won,
+            'winners': get_clap_winners_today(),
+        }
+        return jsonify(data)
+
+def get_clap_winners_today():
+    puzzleNumber = get_puzzle_number()
+    return get_clap_winners(puzzleNumber)
+
+def get_clap_winners(day):
+    con, cur = connect_to_db(STAT_DB_PATH)
+    table_name = f'clap_day{day}'
+    query = f"create table if not exists {table_name}"
+    query += "(unique_hash TEXT PRIMARY KEY, ip_hash TEXT, user_hash TEXT, nb_guesses INT, nb_title_guesses INT, points INT)"
+    cur.execute(query)
+    con.commit()
+    total_winners = -1
+    with con:
+        query = f"SELECT COUNT(*) FROM {table_name}"
+        cur.execute(query)
+        res = cur.fetchall()
+        total_winners = res[0][0] 
+    return total_winners
+
 
 @clap_bp.route('/check_title')   
 def check_title():
